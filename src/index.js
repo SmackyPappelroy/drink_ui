ec5a0951f3f511ea7e60f4afceff68845edd593c
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-03-18 18:35:55 +0100

    Test pull request

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ root.render(
   </React.StrictMode>
 )
 
-// test pull request
+// test pull request again
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
