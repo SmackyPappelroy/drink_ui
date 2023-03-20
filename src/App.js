c4bb166c8bf5c051a4a9c4ad63d1c120a7aae319
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-03-20 10:54:51 +0100

    Fixat bakgrundsbild

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className="main"></div>
+  return (
+    <div className="main">
+      <h1>Test</h1>
+    </div>
+  )
 }
 
 export default App
