dc1ebf906cb68820e144a3e7c0c6d8edc04fc83d
Woffel <hannes.lindros@gmail.com>
2023-03-20 08:38:25 +0100

    fix?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  return <div className="main"></div>
+  return (
+  <>
+    <div className="main">
+      <h1>Hello World!</h1>
+    </div>
+  </>
+)
 }
 
 export default App
