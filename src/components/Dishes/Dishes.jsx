fd52ad30cedd162525e9c8d9e292763d418de403
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-04-04 21:36:09 +0200

    Påbörjat filter-funktion

--- src/components/Dishes/Dishes.jsx
+++ src/components/Dishes/Dishes.jsx
@@ -12,6 +12,27 @@ function Dishes() {
   const { recipes, loading, isMobileSize } = useGlobalContext()
   const [start, setStart] = React.useState(0)
   const [end, setEnd] = React.useState(10)
+  const [filteredRecipes, setFilteredRecipes] = React.useState([])
+  const [vegan, setVegan] = React.useState(false)
+  const [dairyFree, setDairyFree] = React.useState(false)
+  const [glutenFree, setGlutenFree] = React.useState(false)
+  const [ketogenic, setKetogenic] = React.useState(false)
+  const [vegetarian, setVegetarian] = React.useState(false)
+  const [veryHealthy, setVeryHealthy] = React.useState(false)
+  const [veganRecipes, setVeganRecipes] = React.useState([])
+  const [dairyFreeRecipes, setDairyFreeRecipes] = React.useState([])
+  const [glutenFreeRecipes, setGlutenFreeRecipes] = React.useState([])
+  const [ketogenicRecipes, setKetogenicRecipes] = React.useState([])
+  const [vegetarianRecipes, setVegetarianRecipes] = React.useState([])
+  const [veryHealthyRecipes, setVeryHealthyRecipes] = React.useState([])
+
+  useEffect(() => {
+    setFilteredRecipes(recipes)
+  }, [recipes])
+
+  useEffect(() => {
+    updateFilteredRecipes()
+  }, [vegan, dairyFree, glutenFree, ketogenic, vegetarian, veryHealthy])
 
   const increment = () => {
     setStart(start + 10)
@@ -30,13 +51,103 @@ function Dishes() {
     }
   }
 
+  const updateFilteredRecipes = async () => {
+    if (
+      !vegan &&
+      !dairyFree &&
+      !glutenFree &&
+      !ketogenic &&
+      !vegetarian &&
+      !veryHealthy
+    ) {
+      setFilteredRecipes(recipes)
+      console.log('test2')
+      return
+    }
+    if (vegan) {
+      setVeganRecipes(recipes.filter((recipe) => recipe.vegan === true))
+    } else {
+      setVeganRecipes([])
+    }
+    if (dairyFree) {
+      setDairyFreeRecipes(recipes.filter((recipe) => recipe.dairyFree === true))
+    } else {
+      setDairyFreeRecipes([])
+    }
+    if (glutenFree) {
+      setGlutenFreeRecipes(
+        recipes.filter((recipe) => recipe.glutenFree === true)
+      )
+    } else {
+      setGlutenFreeRecipes([])
+    }
+    if (ketogenic) {
+      setKetogenicRecipes(recipes.filter((recipe) => recipe.ketogenic === true))
+    } else {
+      setKetogenicRecipes([])
+    }
+    if (vegetarian) {
+      setVegetarianRecipes(
+        recipes.filter((recipe) => recipe.vegetarian === true)
+      )
+    } else {
+      setVegetarianRecipes([])
+    }
+    if (veryHealthy) {
+      setVeryHealthyRecipes(
+        recipes.filter((recipe) => recipe.veryHealthy === true)
+      )
+    } else {
+      setVeryHealthyRecipes([])
+    }
+
+    let filtered = [
+      ...veganRecipes,
+      ...dairyFreeRecipes,
+      ...glutenFreeRecipes,
+      ...ketogenicRecipes,
+      ...vegetarianRecipes,
+      ...veryHealthyRecipes,
+    ]
+
+    if (filtered.length === 0) {
+      setFilteredRecipes(recipes)
+    } else {
+      setFilteredRecipes(filtered)
+    }
+    console.log(filtered.length)
+  }
+
+  const filterFood = (filter) => {
+    console.log(filter)
+    if (filter === 'vegan') {
+      console.log(vegan)
+      setVegan(!vegan)
+      console.log(vegan)
+    }
+    if (filter === 'dairyFree') {
+      setDairyFree(!dairyFree)
+    }
+    if (filter === 'glutenFree') {
+      setGlutenFree(!glutenFree)
+    }
+    if (filter === 'ketogenic') {
+      setKetogenic(!ketogenic)
+    }
+    if (filter === 'vegetarian') {
+      setVegetarian(!vegetarian)
+    }
+    if (filter === 'veryHealthy') {
+      setVeryHealthy(!veryHealthy)
+    }
+  }
+
   const changePage = (index) => {
     setStart(index)
     setEnd(index + 10)
   }
 
-  console.log('Laddar' + loading)
-  if (loading) {
+  if (loading || !filteredRecipes || filteredRecipes.length === 0) {
     return <h1 className="Laddar"></h1>
   } else {
     return (
@@ -47,15 +158,59 @@ function Dishes() {
             <FontAwesomeIcon icon={Icon.faDrumstickBite} />
             <FontAwesomeIcon icon={Icon.faChampagneGlasses} />
           </h2>
+          <div className="icons">
+            <button
+              className={dairyFree ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('dairyFree')}
+            >
+              dairyFree
+            </button>
+
+            <button
+              className={glutenFree ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('glutenFree')}
+            >
+              glutenFree
+            </button>
+
+            <button
+              className={ketogenic ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('ketogenic')}
+            >
+              ketogenic
+            </button>
+
+            <button
+              className={vegetarian ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('vegetarian')}
+            >
+              vegetarian
+            </button>
+
+            {/* vegan */}
+            <FontAwesomeIcon
+              className={vegan ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('vegan')}
+              icon={Icon.faCarrot}
+            />
+            {/* veryHealthy */}
+            <FontAwesomeIcon
+              className={veryHealthy ? 'icon-clicked' : 'icon-unclicked'}
+              onClick={() => filterFood('veryHealthy')}
+              icon={Icon.faBowlFood}
+            />
+          </div>
         </div>
 
-        {recipes.slice(start, end).map((recipe) => {
-          console.log(recipe)
+        {filteredRecipes.slice(start, end).map((recipe) => {
+          console.log(filteredRecipes)
           return (
             <SimpleDishCard
               key={recipe.id}
               className="simpleDishCard"
-              {...recipe}
+              image={recipe.image}
+              title={recipe.title}
+              id={recipe.id}
             />
           )
         })}
@@ -69,10 +224,11 @@ function Dishes() {
         {/* Display a dot for each page, and highlight the current page */}
         <div className="page">
           <p>
-            {start + 1} - {end} av {recipes.length}
+            {start + 1} - {end} av {filteredRecipes.length}
           </p>
           <div className="dots">
-            {recipes.slice(start, end).map((recipe, index) => {
+            {filteredRecipes.slice(start, end).map((recipe, index) => {
+              console.log(end)
               return (
                 <span
                   key={index}
