4817d4d7119ffa6541cfdf370d26a1ddd2196c2f
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-04-05 20:41:48 +0200

    Tagit bort martins ändringar.

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,26 +1,20 @@
-import React from "react";
-import useNavbarDisplay from "./CustomHooks/useNavbarDisplay";
-import MobileNav from "./Mobile/mobileNavbar";
-import DesktopNav from "./Desktop/desktopNavbar";
-import useCollapse from "./CustomHooks/useCollapse";
-
+import React from 'react'
+import useNavbarDisplay from './CustomHooks/useNavbarDisplay'
+import MobileNav from './Mobile/mobileNavbar'
+import DesktopNav from './Desktop/desktopNavbar'
+import useCollapse from './CustomHooks/useCollapse'
 
 function Navbar() {
-       const isMobile = useNavbarDisplay();
-       const{isCollapsed, toggleCollapse, setToCollapse} = useCollapse();
-   return <>
-    {
-    isMobile ? (<MobileNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} setToCollapse={setToCollapse}/> ) 
-    : (<DesktopNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} setToCollapse={setToCollapse}/>)
-    }
- </>             
-};
-
+  const isMobile = useNavbarDisplay()
   const { isCollapsed, toggleCollapse, setToCollapse } = useCollapse()
   return (
     <>
       {isMobile ? (
-        <MobileNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} />
+        <MobileNav
+          isCollapsed={isCollapsed}
+          toggleCollapse={toggleCollapse}
+          setToCollapse={setToCollapse}
+        />
       ) : (
         <DesktopNav
           isCollapsed={isCollapsed}
@@ -31,5 +25,4 @@ function Navbar() {
     </>
   )
 }
-
 export default Navbar
