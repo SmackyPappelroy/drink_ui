976b184d59489f94e33248d9089ed626d0cf0ec5
WilliamEklundITHS <william.eklund@iths.se>
2023-04-05 15:27:45 +0200

    Stylat navbar och ändrat NavList komponent för bättre flexibilitet

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,17 +1,17 @@
 // Navbar.js
 import React from "react";
-import "./navbar.css";
 import useNavbarDisplay from "./CustomHooks/useNavbarDisplay";
 import MobileNav from "./Mobile/mobileNavbar";
 import DesktopNav from "./Desktop/desktopNavbar";
 import useCollapse from "./CustomHooks/useCollapse";
 
-const Navbar = () => {
+
+function Navbar() {
        const isMobile = useNavbarDisplay();
        const{isCollapsed, toggleCollapse, setToCollapse} = useCollapse();
    return <>
     {
-    isMobile ? (<MobileNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse}/> ) 
+    isMobile ? (<MobileNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} setToCollapse={setToCollapse}/> ) 
     : (<DesktopNav isCollapsed={isCollapsed} toggleCollapse={toggleCollapse} setToCollapse={setToCollapse}/>)
     }
  </>             
