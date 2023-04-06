fe58a74731d800d94ea5816d0284f9dee782a1dc
CoolPlayersAction52 <112393881+CoolPlayersAction52@users.noreply.github.com>
2023-04-06 22:08:28 +0900

    Mobile View Added About & Contact

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -13,8 +13,19 @@ const About = () => {
   return (
     
     <div className={isMobile ? 'card-mobile' : 'card'}>
-      {isMobile ? (
+      {isMobile ? (<>
         <h2>About</h2>
+        <div className="infotext">
+        <p>Our idea is try to find best combonation of dishes that fit well to certain drinks.
+          wine with steak, chicken. </p>
+            <p> Some special dishes that fit well to certain drink.</p>
+            <br></br>
+            <img src={friesbeer} alt="my-gif" className='fooddrink'/>
+            <img src={chickenbeer} alt="my-gif" className='fooddrink'/>
+            <img src={salmonwine} alt="my-gif" className='fooddrink'/>
+            <img src={pouringbeer} alt="my-gif" className='fooddrink'/>
+            </div>
+        </>
       ) : (
         <>
           <PageHeader iconSize="3x" titleSize="50px" /> <h2>About</h2>{' '}
