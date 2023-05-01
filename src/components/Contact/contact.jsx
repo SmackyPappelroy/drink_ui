174b021b3cfb8bb99387ee4ea9fd070f6f614270
WilliamEklundITHS <william.eklund@iths.se>
2023-05-01 15:19:27 +0200

    fixat responsiviteten för det som inte rör api:n och fixat logotyp duplikat

--- src/components/Contact/contact.jsx
+++ src/components/Contact/contact.jsx
@@ -1,47 +1,36 @@
-import PageHeader from '../PageHeader/pageHeader'
-import useNavbarDisplay from '../Navbar/CustomHooks/useNavbarDisplay'
-import '../Card.css'
-import '../../App.css'
-import React, { useEffect } from 'react'
-import cheerschampagne from './cheerschampagne.gif'
-import cheersbeer from './cheersbeer.gif'
-import cheerswine from './cheerswine.gif'
-
+import PageHeader from "../PageHeader/pageHeader";
+import useNavbarDisplay from "../Navbar/CustomHooks/useNavbarDisplay";
+import "../Card.css";
+import "../../App.css";
+import React, { useEffect } from "react";
+import cheerschampagne from "./cheerschampagne.gif";
+import cheersbeer from "./cheersbeer.gif";
+import cheerswine from "./cheerswine.gif";
+import "./contact.css";
 const Contact = () => {
-  const isMobile = useNavbarDisplay()
+  const isMobile = useNavbarDisplay();
   return (
-    <div className={isMobile ? 'card-mobile' : 'card'}>
+    <section className={isMobile ? "card-mobile" : "card"}>
       {isMobile ? (
-        <>
-          <h2>Contact</h2>
-          <div className="infotext">
-            <p>
-              Contact us for more information about the best pairing for dishes
-              and drinks.{' '}
-            </p>
-            <br></br>
-            <img src={cheersbeer} alt="my-gif" className="mobiledrink" />
-            <img src={cheerswine} alt="my-gif" className="mobiledrink" />
-            <img src={cheerschampagne} alt="my-gif" className="mobiledrink" />
-          </div>
-        </>
+        <></>
       ) : (
         <>
-          {' '}
-          <PageHeader iconSize="3x" titleSize="50px" /> <h2>Contact</h2>{' '}
-          <div className="infotext">
-            <p>
-              Contact us for more information about the best pairing for dishes
-              and drinks.{' '}
-            </p>
-            <br></br>
-            <img src={cheersbeer} alt="my-gif" className="drink" />
-            <img src={cheerswine} alt="my-gif" className="drink" />
-            <img src={cheerschampagne} alt="my-gif" className="drink" />
-          </div>
+          <PageHeader iconSize="3x" titleSize="50px" />
         </>
       )}
-    </div>
-  )
-}
-export default Contact
+      <h2>Contact</h2>
+      <div className="infotext">
+        <p>
+          Contact us for more information about the best pairing for dishes and
+          drinks.
+        </p>
+        <div className="images-container">
+          <img src={cheersbeer} alt="my-gif" className="mobiledrink" />
+          <img src={cheerswine} alt="my-gif" className="mobiledrink" />
+          <img src={cheerschampagne} alt="my-gif" className="mobiledrink" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Contact;
--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -1,42 +1,49 @@
-import React, { useEffect } from 'react'
-import { useGlobalContext } from '../../context'
-import './Dish.css'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import * as Icon from '@fortawesome/free-solid-svg-icons'
-import '../../App.css'
+import React, { useEffect } from "react";
+import { useGlobalContext } from "../../context";
+import "./Dish.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import * as Icon from "@fortawesome/free-solid-svg-icons";
+import "../../App.css";
+import PageHeader from "../PageHeader/pageHeader";
+import useNavbarDisplay from "../Navbar/CustomHooks/useNavbarDisplay";
 
 const findRecipeById = (recipes, id) => {
-  return recipes.find((recipe) => recipe.id === parseInt(id))
-}
+  return recipes.find((recipe) => recipe.id === parseInt(id));
+};
 
 const Dish = () => {
-  const [myRecipe, setMyRecipe] = React.useState(null)
-  const id = window.location.pathname.split('/')[2]
-  const { recipes, loading, isMobileSize } = useGlobalContext()
+  const [myRecipe, setMyRecipe] = React.useState(null);
+  const id = window.location.pathname.split("/")[2];
+  const { recipes, loading, isMobileSize } = useGlobalContext();
 
   useEffect(() => {
-    const recipe = findRecipeById(recipes, id)
-    console.log(recipe)
-    setMyRecipe(recipe)
-  }, [id, recipes])
-
+    const recipe = findRecipeById(recipes, id);
+    console.log(recipe);
+    setMyRecipe(recipe);
+  }, [id, recipes]);
+  const isMobile = useNavbarDisplay();
   if (loading || !recipes || !recipes.length || !id || !myRecipe) {
-    return <h1 className="loading"></h1>
+    return <h1 className="loading"></h1>;
   }
 
   if (!myRecipe) {
-    return null
+    return null;
   }
 
   return (
-    <div className={isMobileSize ? 'dish-mobile' : 'dish'}>
-      <div className="dish-title">
+    <div className={isMobileSize ? "dish-mobile" : "dish"}>
+      {isMobile ? (
+        <></>
+      ) : (
+        <PageHeader titleSize="3.5rem" iconSize="5x"></PageHeader>
+      )}
+      {/* <div className="dish-title">
         <h2 className="fine-dine">
           <div className="title-gray">FineDine</div>
           <FontAwesomeIcon icon={Icon.faDrumstickBite} />
           <FontAwesomeIcon icon={Icon.faChampagneGlasses} />
         </h2>
-      </div>
+      </div> */}
       <div className="dish-info">
         <h1 className="dish-name">{myRecipe.title}</h1>
       </div>
@@ -46,7 +53,7 @@ const Dish = () => {
         {myRecipe.ingredients ? (
           <div>
             {myRecipe.ingredients.length > 800
-              ? myRecipe.ingredients.substring(0, 800) + '...'
+              ? myRecipe.ingredients.substring(0, 800) + "..."
               : myRecipe.ingredients}
           </div>
         ) : (
@@ -60,7 +67,7 @@ const Dish = () => {
             dangerouslySetInnerHTML={{
               __html:
                 myRecipe.instructions.length > 800
-                  ? myRecipe.instructions.substring(0, 800) + '...'
+                  ? myRecipe.instructions.substring(0, 800) + "..."
                   : myRecipe.instructions,
             }}
           ></div>
@@ -82,12 +89,12 @@ const Dish = () => {
                   />
                   <h4 className="drink-name">{drink.name}</h4>
                 </div>
-              )
+              );
             })
-          : 'Hittade inga rekommenderade drycker.'}
+          : "Hittade inga rekommenderade drycker."}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dish
+export default Dish;
