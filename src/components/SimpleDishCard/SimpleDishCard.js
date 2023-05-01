1f770a6d57a06259a34ce1c3f441f68c9d9154f6
SmackyPappelroy <65800614+SmackyPappelroy@users.noreply.github.com>
2023-05-01 13:01:29 +0200

    Lagt till .env fil för api-key. Fixat Dish

--- src/components/SimpleDishCard/SimpleDishCard.js
+++ src/components/SimpleDishCard/SimpleDishCard.js
@@ -6,7 +6,6 @@ import { Link } from 'react-router-dom'
 function SimpleDishCard({ image, title, id }) {
   const { isMobileSize } = useGlobalContext()
   const titleLength = isMobileSize ? 30 : 40
-
   return (
     <Link to={`/dish/${id}`} className="simpleDishCard">
       <img className="simpleDishCardImage" src={image} alt={title}></img>
